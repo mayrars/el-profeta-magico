6ad24b68afd646369939219a3bd078b42033cef7
Mayra <mayra203@gmail.com>
2024-04-16 05:59:22 -0600

    preparing environment

---
+++ src/env.d.ts
@@ -0,0 +1,17 @@
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+interface ImportMetaEnv {
+  /**
+   * Built-in environment variable.
+   * @see Docs https://github.com/chihab/dotenv-run/packages/angular#node_env.
+   */
+  readonly NODE_ENV: string;
+  // Add your environment variables below
+  // readonly NG_APP_API_URL: string;
+  [key: string]: any;
+  readonly NG_APP_KEY: string;
+  readonly NG_APP_URL: string;
+}
+
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    apiKey: import.meta.env.NG_APP_KEY,
+    apiUrl: import.meta.env.NG_APP_URL,
+};
