875009e7bb3b3abf0a8764beda5ee2bbc4580afd
Mayra <mayra203@gmail.com>
2024-04-29 04:50:13 -0600

    Get categories in home section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,20 +1,52 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 import { Article } from '../../models/article.model';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
   private _apiService = inject(ApiService);
   topNews:Article[] = []
+  businessNews:Article[] = []
+  technologyNews:Article[] = []
+  entertainmentNews:Article[] = []
+  healtNews:Article[] = []
+  scienceNews:Article[] = []
+  sportsNews:Article[] = []
+  constructor() { }
   ngOnInit(): void {
-    this._apiService.getTopHeadlines().subscribe(res => {
-      this.topNews = res.articles;
+    this._apiService.getnewsByCategory('general').subscribe(res => {
+      this.topNews = res.articles.slice(0,3);
+    })
+    //get the busines news
+    this._apiService.getnewsByCategory('business').subscribe(res => {
+      this.businessNews = res.articles.slice(0,4);
+    })
+    //get the technology news
+    this._apiService.getnewsByCategory('technology').subscribe(res => {
+      this.technologyNews = res.articles.slice(0,3);
+    })
+    //get entertainment news
+    this._apiService.getnewsByCategory('entertainment').subscribe(res => {
+      this.entertainmentNews = res.articles.slice(0,3);
+    })
+    //get healt news
+    this._apiService.getnewsByCategory('health').subscribe(res => {
+      this.healtNews = res.articles.slice(0,3)
+    })
+    //get the science news
+    this._apiService.getnewsByCategory('science').subscribe(res => {
+      this.scienceNews = res.articles.slice(0,4 )
+    })
+    //get sport news
+    this._apiService.getnewsByCategory('sports').subscribe(res => {
+      this.sportsNews = res.articles.slice(0,4)
     })
   }
 }
