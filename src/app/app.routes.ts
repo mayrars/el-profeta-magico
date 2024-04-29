b7231c2792a88c38d5a879d80a859c2bd64896d7
Mayra <mayra203@gmail.com>
2024-04-29 06:58:29 -0600

    adding new routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { ArticleComponent } from './pages/article/article.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     {path:'', component:HomeComponent},
-    {path:'**', component:HomeComponent}
+    {path:'category/:title', component:CategoryComponent},
+    {path:'article/:title', component:ArticleComponent},
+    {path:'**', component:PageNotFoundComponent}
 ];
