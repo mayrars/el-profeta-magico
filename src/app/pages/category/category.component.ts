83795093c932a02f36222da400d762ecc1618a92
Mayra <mayra203@gmail.com>
2024-04-30 05:10:29 -0600

    Change ngOninit, get params in category route

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -1,27 +1,34 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute, Params, RouterLink } from '@angular/router';
 import { ApiService } from '../../services/api.service';
 import { Article } from '../../models/article.model';
-import { TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-category',
   standalone: true,
-  imports: [RouterLink,UpperCasePipe],
+  imports: [RouterLink,TitleCasePipe],
   templateUrl: './category.component.html',
   styleUrl: './category.component.scss'
 })
 export class CategoryComponent implements OnInit{
-  private _router = inject(ActivatedRoute)
+  private _route = inject(ActivatedRoute)
   private _apiService = inject(ApiService);
   category: string = ''
   newsCategory: Article[] = []
-  ngOnInit(): void {
-    this.category = this._router.snapshot.params['title']
-    //get news by category name
-    this._apiService.getnewsByCategory(this.category).subscribe(data => {
-      this.newsCategory = data.articles
-      console.log(this.newsCategory)
+  ngOnInit(): void {    
+    this._route.params.subscribe({
+      next: (params: Params)=>{
+        //get news by category name
+        this.category = params['title']
+        this._apiService.getnewsByCategory(params['title']).subscribe(data => {
+          this.newsCategory = data.articles
+          console.log(this.newsCategory)
+        })
+      },
+      error: (err: any)=>{
+        console.log(err)
+      }
     })
   }
 }
