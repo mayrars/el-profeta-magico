9bc758f06ecc6d8e09caaaca87627185474e7d7d
Mayra <mayra203@gmail.com>
2024-05-01 06:43:30 -0600

    Change service link

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,7 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
-import { Article } from '../models/article.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +11,6 @@ export class ApiService {
   private apiUrl = environment.apiUrl;
   //get news by category
   getnewsByCategory(category:string):Observable<any> {
-    return this.http.get(`${this.apiUrl}/top-headlines?country=us&category=${category}&apiKey=${environment.apiKey}`)
+    return this.http.get(`${this.apiUrl}top-headlines?country=us&category=${category}&apiKey=${environment.apiKey}`)
   }
 }
