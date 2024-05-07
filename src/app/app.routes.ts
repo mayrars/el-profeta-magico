aaf3f3e34eb688d05e7cc9f310050a738ad4e5b2
Mayra <mayra203@gmail.com>
2024-05-07 06:12:00 -0600

    create contact route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { CategoryComponent } from './pages/category/category.component';
 import { ArticleComponent } from './pages/article/article.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 export const routes: Routes = [
     {path:'', component:HomeComponent},
     {path:'category/:title', component:CategoryComponent},
     {path:'article/:title', component:ArticleComponent},
+    {path:'contact',component:ContactComponent},
     {path:'**', component:PageNotFoundComponent}
 ];
